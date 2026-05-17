bedeb0e710e9aafa59db30b901225033188b725e
Kokc80 <kokc@inbox.ru>
2026-05-18 07:40:40 +0900

    задание 30

---
+++ lms/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from lms.models import Course
+from lms.serializer import CourseSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+
+# Create your views here.
