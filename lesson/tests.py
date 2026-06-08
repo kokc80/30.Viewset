4ea204644572ee5565d2d022bedd64bd7b3bcbbc
Kokc80 <kokc@inbox.ru>
2026-06-09 05:34:48 +0900

    20260609 тестирование

--- lesson/tests.py
+++ lesson/tests.py
@@ -1,9 +1,10 @@
+from idlelib.rpc import response_queue
+
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
 from users.models import User
 from lesson.models import Course, Lesson
-from rest_framework.test import force_authenticate
 
 
 class LessonTestCase(APITestCase):
@@ -14,11 +15,27 @@ class LessonTestCase(APITestCase):
         self.lesson = Lesson.objects.create(name = "Тестовый урок", course = self.course, owner = self.user, descr = "описание")
         self.client.force_authenticate(user=self.user)
 
-    def test_course_retrieve(self):
-        url = reverse("lessons:course-retrieve", args=(self.course.pk,))
-        response = self.client(url)
+    def test_lesson_retrieve(self):
+        url = reverse("lessons:lessons_retrieve", args=(self.lesson.pk,))
+        response = self.client.get(url)
         data = response.json()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get("name"), self.lesson.name)
+        #  спросить как сравнивать?
+        # self.assertEqual(data.get("course"), self.lesson.course)
+
+    def test_lesson_create(self):
+        url = reverse("lessons:lessons_create")
+        course_t = self.course
+        data = {
+            "name": "Тестовый урок",
+            "descr": "Описание",
+            "course": "Тестовый курс",
+            "owner": self.user
+        }
+
+        response =self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
 
