7d676e81ca1eb26c5890b8eb72f07586e31beb31
Kokc80 <kokc@inbox.ru>
2026-06-11 13:40:00 +0900

    20260611  2 Задание ошибка line_items=[{"price": price.get("id"), "quantity": 1}],

--- users/services.py
+++ users/services.py
@@ -29,7 +29,7 @@ def create_stripe_session( price):
 
     session = stripe.checkout.Session.create(
         success_url = "https://127.0.0.1:8000/",
-        line_items = [{"price": price.get("id"), "quantity": 1}],
-        mode = "payment",
+        line_items=[{"price": price.get("id"), "quantity": 1}],
+        mode="payment",
     )
     return session.get("id"), session.get("url")
