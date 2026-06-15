c2e29594da3e5dc2331a063c25e0e2024ae01fc3
Kokc80 <kokc@inbox.ru>
2026-06-16 06:15:03 +0900

    20260616 задание 33 Celery Ошибка kombu.exceptions.EncodeError: Object of type Course is not JSON serializable

--- lesson/tasks.py
+++ lesson/tasks.py
@@ -1,7 +1,10 @@
+from datetime import timezone, timedelta
+
 from celery import shared_task
 from django.core.mail import send_mail
 from lesson.models import Course, SubscriptionCourse
 from config.settings import EMAIL_HOST_USER
+from users.models import User
 
 
 @shared_task
@@ -18,3 +21,17 @@ def mail_update_course_info(course_id):
             recipient_list=[subscription.user.email],
             fail_silently=False
         )
+
+
+@shared_task
+def check_last_login():
+    """Проверка последнего входа пользователей и отключение неактивных пользователей"""
+    users = User.objects.filter(last_login__isnull=False)
+    today = timezone.now()
+    for user in users:
+        if today - user.last_login > timedelta(days=30):
+            user.is_active = False
+            user.save()
+            print(f'Пользователь {user.email} отключен')
+        else:
+            print(f'Пользователь {user.email} активен')
