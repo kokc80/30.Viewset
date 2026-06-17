1dbe5e2d665a8bd0dc05ce6027c511cfcf2f76d2
Kokc80 <kokc@inbox.ru>
2026-06-17 19:27:22 +0900

    20260616 задание 33 Celery исправил ошибку

--- config/settings.py
+++ config/settings.py
@@ -165,10 +165,13 @@ STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
 
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+
 CElERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT =30*60
-CELERY_RESULT_BACKEND="redis:/localhost:6379/0"
+
+CELERY_RESULT_BACKEND="redis://localhost:6379/0"
+
 #
 # CORS_ALLOWED_ORIGINS = [
 #     '<http://localhost:8000>',  # Замените на адрес вашего фронтенд-сервера
